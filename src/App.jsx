b5aa1b9564d7398a2dce3783ce0bf637a305abf9
VittoriaBasile <vichy.basile@gmail.com>
2023-03-30 16:46:32 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import AllTheBook from "./components/AllTheBooks";
 import MyFooter from "./components/MyFooter";
 import MyNav from "./components/MyNav";
 import Welcome from "./components/Welcome";
+import books from "./Books/horror.json";
+import BookList from "./components/BookList";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <MyNav />
       <Welcome />
-      <AllTheBook />
+
+      <Container fluid>
+        <BookList books={books} />
+      </Container>
       <MyFooter />
     </div>
   );
